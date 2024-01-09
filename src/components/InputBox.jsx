574b0ba9987f65e78e1a08944649f5d126d05ebf
nooralamf42 <nooralamf42@gmail.com>
2024-01-09 23:01:32 +0530

    still working

--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,44 +1,48 @@
+import React, {useId} from 'react'
 
 function InputBox({
     label,
     amount,
-    amountChange,
-    currency='usd',
-    currencyChange,
-    options = [],
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountDisable = false,
+    currencyDisable = false,
     className = "",
 }) {
-   
-    // console.log(amount)
+   const amountInputId = useId()
+
     return (
-        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
             <div className="w-1/2">
-                <label  className="text-black/40 mb-2 inline-block">
+                <label htmlFor={amountInputId}  className="text-black/40 mb-2 inline-block">
                     {label}
                 </label>
                 <input
+                    id={amountInputId}
                     className="outline-none w-full bg-transparent py-1.5"
                     type="number"
                     placeholder="Amount"
+                    disabled={amountDisable}
                     value={amount}
-                    onInput={(e)=>amountChange && amountChange(Number(e.target.value))}
+                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
-                    value={currency}
-                    onChange={(e)=>currencyChange && currencyChange(e.target.value)}
+                    value={selectCurrency}
+                    onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
                 >
                     
-                {
-                    options.map((optionName)=>(
-                        <option key={optionName} value="usd">
-                            {optionName}
-                        </option>
-                    ))
-                }
+                        {currencyOptions.map((currency) => (
+                            <option key={currency} value={currency}>
+                            {currency}
+                            </option>
+                        ))}
                 
                 </select>
             </div>
